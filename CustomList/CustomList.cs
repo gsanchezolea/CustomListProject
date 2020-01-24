8154ad5492148fb7889a4a18ec37eea14ee68566
unknown <thetavog@gmail.com>
2020-01-24 12:44:49 -0600

    created an add method for the custom list, added an index in order to retrieve and set the values of the slots in the array

--- CustomList/CustomList.cs
+++ CustomList/CustomList.cs
@@ -6,7 +6,64 @@ using System.Threading.Tasks;
 
 namespace CustomList
 {
-    class CustomList
+    public class CustomList<T>
     {
+        T[] array;
+        public int Count;
+        public int Capacity;
+        public int AddedCapacity;
+        
+
+
+        public CustomList()
+        {
+            Capacity = 4;
+            Count = 0;
+            AddedCapacity = 2;
+
+            array = new T[Capacity];            
+        }
+
+        //Member Variables
+        public void Add(T parameter)
+        {
+
+            if (Count == Capacity)
+            {
+                IncreaseCapacity();
+                CopyTo();
+               
+            }
+            array[Count] = parameter;
+            Count++;
+
+        }
+        public void IncreaseCapacity()
+        {
+            Capacity *= AddedCapacity;
+        }
+        public void CopyTo()
+        {
+            T[] collection = array;
+            array = new T[Capacity];
+            for (int i = 0; i < Count; i++)
+            {
+                array[i] = collection[i];
+            }
+        }
+        public T this[int i]
+        {
+            get 
+            { 
+                return array[i]; 
+            }
+            set 
+            {
+                array[i] = value; 
+            }
+        }
     }
 }
+
+
+
